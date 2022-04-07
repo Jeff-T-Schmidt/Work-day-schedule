0a7d67514639201274f390aed11705e8ec5970dc
Jeff Schmidt <jeff.t.schmidt@gmail.com>
2022-04-07 16:07:59 -0700

    Final

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Work-day-schedule
 05 Third-Party APIs: Work Day Scheduler
+
+This is the Work Day Scheduler. You can input text in the field and when you refresh the page, what you wrote is still there.
+
+Sceenshot of the website:
+![image](https://user-images.githubusercontent.com/101135703/162333627-e39b1b24-a7f0-4c3e-8f46-35a97a559f0e.png)
+
+URL: https://jeff-t-schmidt.github.io/Work-day-schedule/
--- script.js
+++ script.js
@@ -15,41 +15,32 @@ function saveSched() {
 
     localStorage.setItem(hour, toDo)
 }
-
-
-
-
-
-
-
-
-
 console.log(moment().format('HH a'))
 saveBtnEl.click(saveSched)
 
-function timeBlock() {
-    var hours_Day = 24;
-    let hours_Start = 9;
-    let hours_End = 17;
-    var now = moment().text(currentTime).format('h')
-    var div = document.createElement("div");
-    for (let i = 0; i < hours_Day; i++) {
-        console.log(now)
-        if (i >= hours_Start && i <= hours_End) {
+// function timeBlock() {
+//     var hours_Day = 24;
+//     let hours_Start = 9;
+//     let hours_End = 17;
+//     var now = moment().text(currentTime).format('h')
+//     var div = document.createElement("div");
+//     for (let i = 0; i < hours_Day; i++) {
+//         console.log(now)
+//         if (i >= hours_Start && i <= hours_End) {
 
-            // Past-Tense
-            if (now() > i) {
-                div.setAttribute("class", "past row time-block");
-            }
-            // Present-Tense
-            else if (now() == i) {
-                div.setAttribute("class", "present row time-block");
-            }
-            // Future-Tense
-            else {
-                div.setAttribute("class", "future row time-block");
-            }
-        }
-    }
-}
-timeBlock()
+//             // Past-Tense
+//             if (now() > i) {
+//                 div.setAttribute("class", "past row time-block");
+//             }
+//             // Present-Tense
+//             else if (now() == i) {
+//                 div.setAttribute("class", "present row time-block");
+//             }
+//             // Future-Tense
+//             else {
+//                 div.setAttribute("class", "future row time-block");
+//             }
+//         }
+//     }
+// }
+// timeBlock()
