af6de59bee2e319d25858dd9f06f913317b71f19
Jeff Schmidt <jeff.t.schmidt@gmail.com>
2022-04-06 23:58:28 -0700

    Wednesday

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMM Do"));
 
+var saveBtnEl = $('.saveBtn')
+
+function saveSched(){
+    var toDo = $(this).siblings("input").val()
+    var hour = $(this).siblings("div").text()
+
+    localStorage.setItem(hour, toDo)
+}
+console.log(moment().format('HH a'))
+saveBtnEl.click(saveSched)
